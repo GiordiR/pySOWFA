314382a52b9ac864f2455ca477480dd5cd3f2044
GiordiR <riki.giordi@hotmail.it>
2022-10-21 17:26:55 +0200

    updated tutorial

--- tutorials/application_steady.py
+++ tutorials/application_steady.py
@@ -4,13 +4,18 @@ from pySOWFA import *
 
 def steady_flex():
     turbName = 'turbine0'
-    turbineDir = '../../WTMresults/WTM_TURBULENT_fine_Rated_rigid'
     turbineFileName = 'DTU10MW_POLIMI_WTM'
+    turbineDirName = 'WTM_TURBULENT_fine_Rated_rigid'
 
+    # Set directories paths
+    path = os.getcwd()
+    turbineDir = os.path.abspath(os.path.join(path, os.pardir, 'WTMresults', turbineDirName))
     postProcDir = os.path.join(turbineDir, 'postProcessing')
+    expDir = os.path.abspath(os.path.join(path, os.pardir, 'UNAFLOW', 'WAKE'))
 
     # probe reference wind streamwise velocity
     probeName = 'probe_URef'
     pURef = OpenFOAM(turbName, probeName, turbineDir, turbineFileName)
-    pURef.readWakeExperiment()
+    pURef.readWakeExperiment(expDir=expDir)
 
+    pURef.readSets(postProcDir=postProcDir, var='Ux')
